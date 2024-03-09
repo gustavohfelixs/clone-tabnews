2499a966a7d44cf361bfe937ed4d9b1078767ba8
Gustavo Félix <99498636+gustavohfelixs@users.noreply.github.com>
2024-03-09 16:25:24 +0000

    Adicionando tela de em breve

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,51 @@
 function Home() {
   return (
-    <h1>
-      Nilessa meu amor, eu te amo! Você é muito especial para mim e me inspira,
-      se você me ama imita uma arara! :D
-    </h1>
+    <div style={{ backgroundColor: "RGB(22,22,22)" }}>
+      <div style={{ textAlign: "center", display: "block" }}>
+        <img
+          src="https://drive.google.com/thumbnail?id=1GWOzsyrFbhQvqZg8z7cm08IDLIO6xJ-D&sz=w1000"
+          alt="react logo"
+          style={{
+            width: "50%",
+            margin: "auto",
+            justifyContent: "center",
+            alignContent: "center",
+            alignItems: "center",
+          }}
+        />
+        <h1
+          style={{
+            textAlign: "center",
+            color: "white",
+            fontFamily: "Poppins",
+            fontStyle: "normal",
+            fontWeight: "lighter",
+          }}
+        >
+          Em breve uma nova ferramenta para salvar o tempo de profissionais que
+          trabalham com agendamento, os conectando com aqueles que precisam de
+          seus trabalhos
+        </h1>
+        <h1>.</h1>
+        <h1>.</h1>
+        <h1>.</h1>
+        <h1>.</h1>
+        <h1>.</h1>
+        <h1>.</h1>
+        <h1>.</h1>
+        <h1>.</h1>
+        <h1>.</h1>
+        <h1>.</h1>
+        <h1>.</h1>
+        <h1>.</h1>
+        <h1>.</h1>
+        <h1>.</h1>
+        <h1>.</h1>
+        <h1>.</h1>
+        <h1>.</h1>
+        <h1>.</h1>
+      </div>
+    </div>
   );
 }
 
